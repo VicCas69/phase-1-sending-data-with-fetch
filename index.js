cde106b7544fd4bf343a7d139cc303ad4bac6051
victor castro <victor_castro_01@hotmail.com>
2021-09-14 19:38:49 -0400

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(username,emailAddress){
+    const f = fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            accept : 'application/json',
+        },
+        body: JSON.stringify({
+            name : username,
+            email: emailAddress 
+        })
+        
+    }).then(resp => resp.json())
+    .then(user => {
+        document.body.innerHTML = user.id;
+    })
+    .catch((message => { 
+        document.body.innerHTML = `Error: " ${message}`;
+    }))
+    return f;
+}
+submitData('victor', 'victor@hotmail.com')
